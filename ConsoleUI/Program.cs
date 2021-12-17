b38cd7d36e270b03ce6f0109d859d2cff684632a
barisahmetfdn256 <73822000+barisahmetfdn256@users.noreply.github.com>
2021-12-17 21:59:28 +0300

    up

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,64 +1,19 @@
 ﻿using Business.Concrate;
 using DataAccsess.Concrate.EntityFramework;
-using Etities.Concrate;
 using System;
 
 namespace ConsoleUI
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-
-
-            ColorManager color = new ColorManager(new EfColorDal());
-            foreach (var colors in color.GetById(2))
-            {
-                Console.WriteLine(colors.ColorName);
-            }
-            Console.WriteLine("--------------");
-            Console.WriteLine("--------------");
-            Console.WriteLine("--------------");
-            CostomerManager costomer = new CostomerManager(new EfCostomerDal());
-            foreach (var costomers in costomer.GetById(4))
-            {
-                Console.WriteLine(costomers.CustFirstName);
-            }
-            {
-
-            }
-            Console.WriteLine( "------------");
-            Console.WriteLine( "------------");
-            Console.WriteLine( "------------");
-            BrandManager brand = new    BrandManager(new  EfBrandDal());        
-            foreach (var brands in brand.GetById(5 ))
-            {
-                Console.WriteLine(brands.BrandName);
-            }
-            Console.WriteLine("--------------------");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("--------------------");
             CarManager car = new CarManager(new EfCarDal());
-            foreach (var cars in car.GetByPrice(500,1000))
+            foreach (var cars in car.GettAll())
             {
-                Console.WriteLine(cars.Brand +" " + cars.Color + " " + cars.DailyPrice);
+                Console.WriteLine(cars.Brand +" " + cars.Model + " " + cars.DailyPrice +" " + cars.Color)   ;
             }
-            Console.WriteLine("-----------");
-            Console.WriteLine("-----------");
-            Console.WriteLine("-----------");
-            Console.WriteLine("-----------");
-            foreach (var cars in car.GettAll( ))
-            {
-                Console.WriteLine(cars.Brand + " " + cars.Color + " " + cars.DailyPrice);
-            }
-            Console.WriteLine("--------------------");
-            Console.WriteLine("--------------------");
-            Console.WriteLine("--------------------");
-            MotorcycleManager motorcycle = new MotorcycleManager(new EfMotorcycleDal());
-            foreach (var motors in motorcycle.GettAll())
-            {
-                Console.WriteLine(motors.Brand + " " + motors.Model);
-            }
+            Console.WriteLine("zazazaza");
         }
     }
 }
